a0c3cc3caf577b7360e3f0f68f11bb6a1e0627c2
Mayukh Sinha <mayukh@Mayukhs-MacBook-Pro.local>
2025-02-17 01:35:37 -0500

    A03: Added dynamic cidr and subnet fix

--- AWS-VPC/variables.tf
+++ AWS-VPC/variables.tf
@@ -16,20 +16,8 @@ variable "vpc_cidr" {
   default     = "10.10.0.0/16"
 }
 
-variable "availability_zones" {
-  description = "List of availability zones to use"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+variable "subnet_count" {
+  description = "Number of subnets (1 public & 1 private per AZ)"
+  type        = number
+  default     = 3
 }

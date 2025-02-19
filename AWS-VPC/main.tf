e57d57f0e05b6ae6a43d573b0e4126815b618034
Mayukh Sinha <mayukh@Mayukhs-MacBook-Pro.local>
2025-02-18 20:27:26 -0500

    A03: yml script update

--- AWS-VPC/main.tf
+++ AWS-VPC/main.tf
@@ -12,10 +12,10 @@ data "aws_vpcs" "existing_vpcs" {}
 # Generate Unique VPC Name with Incrementing Counter
 locals {
   base_vpc_name = "Demo-VPC"
-  
+
   # Count how many existing VPCs start with "Demo-VPC"
   existing_vpc_count = length(data.aws_vpcs.existing_vpcs.ids) + 1
-  
+
   # Generate a unique name by appending the counter
   unique_vpc_name = "${local.base_vpc_name}-${local.existing_vpc_count}"
 
